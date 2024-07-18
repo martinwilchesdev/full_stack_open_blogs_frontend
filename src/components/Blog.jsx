5b2b27978b4513b6f19ebfba036629498af51c19
Martin Wilches <martindotdev@gmail.com>
2024-07-18 11:27:59 -0500

    Estilos en linea añadidos para la informacion principal de cada blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,16 @@
 const Blog = (props) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
     const buttonLabel = props.blogId === props.blog.id.toString() ? 'hide' : 'view'
 
     return (
         <>
-            <div>
+            <div style={blogStyle}>
                 <span>{props.blog.title} {props.blog.author}</span>
                 <button onClick={props.onHandleViewInfoBlog}>{buttonLabel}</button>
             </div>
